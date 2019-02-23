5d3ae941f48e1219fa64cbd1c16140308b083e5b
arsen <arsenchechen@yahoo.com>
2019-02-23 23:43:53 +0500

    changed sending parameters to more convenient

--- src/lib.rs
+++ src/lib.rs
@@ -39,10 +39,18 @@ pub struct VkData {
 
 impl VkData {
 
-	fn get(&self, method: &str, params: String) -> Result<Value, Box<Error>> {
+	fn method(&self, method: &str, params: std::vec::Vec<(&str, &str)>) -> 
+		Result<Value, Box<Error>> {
 		let unk_err = "Unknown json from vk";
+
+		let mut text = String::new();
+		for p in params.iter() {
+			text = text + p.0 + "=" + p.1 + "&";
+		}
+		text += &format!("access_token={AT}&v={V}", AT=&self.access_token, V=&self.version);
+
 		let data:Value = reqwest::get(
-			format!("{Url}{Method}?{Params}", Url=&self.url, Method=method, Params=params)
+			format!("{Url}{Method}?{Params}", Url=&self.url, Method=method, Params=text)
 		.as_str())?
 		.json()?;
 
@@ -57,37 +65,29 @@ impl VkData {
 
 	#[allow(non_snake_case)]
 	pub fn messages_getHistory(&self, user_id: i64) -> Result<Value, Box<Error>> {
-		Ok( self.get("messages.getHistory", 
-			format!("count={C}&user_id={UI}&group_id={GI}&access_token={AT}&v={V}",
-				C=1, UI=user_id, GI=self.group_id, AT=self.access_token, V=self.version
-			)
+		Ok( self.method("messages.getHistory", 
+			vec![("count", "1"), ("user_id", &user_id.to_string()), ("group_id", self.group_id)]
 		)? )
 	}
 
 	pub fn messages_send(&self, message: String, user_id: i64) -> Result<(), Box<Error>> {
 		let x = rand::random::<i32>();
-		self.get("messages.send",
-			format!("user_id={UI}&random_id={RI}&message={M}&dont_parse_link={DPL}&access_token={AT}&v={V}",
-				UI=user_id, RI=x, M=message, DPL=0, AT=self.access_token, V=self.version
-			)
+		self.method("messages.send",
+			vec![("user_id", &user_id.to_string()), ("random_id", &x.to_string()), ("message", &message), ("dont_parse_link", "0")]
 		)?;
 		Ok(())
 	}
 
 	pub fn users_get(&self, user_id: i64) -> Result<Value, Box<Error>> {
-		Ok( self.get("users.get",
-			format!("user_ids={UI}&name_case=Nom&access_token={AT}&v={V}",
-				UI=user_id, AT=self.access_token, V=self.version
-			)
+		Ok( self.method("users.get",
+			vec![("user_ids", &user_id.to_string()), ("name_case", "Nom")]
 		)? )
 	}
 
 	#[allow(non_snake_case)]
 	pub fn groups_GetLongPollServer(&self) -> Result<DataOfServer, Box<Error>> {
-		let resp = self.get("groups.getLongPollServer",
-			format!("group_id={GI}&access_token={AT}&v={V}",
-				GI=self.group_id, AT=self.access_token, V=self.version
-			)
+		let resp = self.method("groups.getLongPollServer",
+			vec![("group_id", self.group_id)]
 		)?;
 		Ok(DataOfServer{
 			key: as_str_handle(&resp["key"])?,
